b4b6127521299b4c0cf21a681176cdbf8add48ac
Evan McNeely <evan.mcneely@leadpages.com>
2023-02-05 10:08:07 -0800

    moved dotfiles directory. updating dotfiles location.

--- scripts/link.sh
+++ scripts/link.sh
@@ -1,4 +1,4 @@
-DOTFILES="${HOME}/dotfiles"
+DOTFILES="${HOME}/dev/dotfiles"
 
 ln -sf "${DOTFILES}/zsh/.zshrc" ~
 ln -sf "${DOTFILES}/vim/.vimrc" ~
@@ -7,3 +7,4 @@ ln -sf "${DOTFILES}/vim/.vim/coc-settings.json" "${HOME}/.vim"
 ln -sf "${DOTFILES}/zsh/evans.zsh-theme" "${HOME}/.oh-my-zsh/themes"
 ln -sf "${DOTFILES}/git/.gitignore_global" ~
 ln -sf "${DOTFILES}/git/.gitconfig" ~
+ln -sf "${DOTFILES}/scripts/nv.sh" "${HOME}/.local/bin/" 
