e85978d884ec0e85898bfc275132f74f8f2e1bfb
Evan McNeely <evan.mcneely@leadpages.com>
2023-02-06 19:11:43 -0800

    renamed some keymaps

--- nvim/lua/plugins/todo-comments.lua
+++ nvim/lua/plugins/todo-comments.lua
@@ -9,7 +9,7 @@ return {
       { "]t", function() require("todo-comments").jump_next() end, desc = "Next ToDo" },
       { "[t", function() require("todo-comments").jump_prev() end, desc = "Previous ToDo" },
       { "<leader>ct", "<cmd>TodoTrouble<cr>", desc = "ToDo (Trouble)" },
-      { "<leader>cT", "<cmd>TodoTelescope<cr>", desc = "ToDo" },
+      { "<leader>cT", "<cmd>TodoTelescope<cr>", desc = "ToDo (Search)" },
     },
   },
 }
