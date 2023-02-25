5dd4c029973886e6529f12871e6b69f318524b56
Evan McNeely <evan.mcneely@leadpages.com>
2023-02-25 09:03:14 -0800

    not doing anything

--- nvim/lua/plugins/toggleterm.lua
+++ nvim/lua/plugins/toggleterm.lua
@@ -7,11 +7,11 @@ return {
     hide_numbers = true,
     open_mapping = [[<C-\>]],
     shade_filetypes = {},
-    shade_terminals = false,
-    shading_factor = 0.6,
+    -- shade_terminals = true,
+    -- shading_factor = -30,
     start_in_insert = true,
     persist_size = true,
-    direction = "float",
+    direction = "horizontal",
     winbar = {
       enabled = false,
       name_formatter = function(term)
