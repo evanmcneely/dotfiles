0ac4e00f38bcb565e0cb29eeca7dca760d9b91e4
Evan McNeely <evan.mcneely@leadpages.com>
2023-07-20 11:37:02 -0700

    nvim stuff

--- nvim/lua/plugins/lsp/diagnostics.lua
+++ nvim/lua/plugins/lsp/diagnostics.lua
@@ -21,13 +21,13 @@ function M.setup()
     },
 
     diagnostic = {
-      -- virtual_text = false,
+      virtual_text = false,
       -- virtual_text = { spacing = 4, prefix = "●" },
-      virtual_text = {
-        severity = {
-          min = vim.diagnostic.severity.ERROR,
-        },
-      },
+      -- virtual_text = {
+        -- severity = {
+          -- min = vim.diagnostic.severity.ERROR,
+        -- },
+      -- },
       signs = {
         active = signs,
       },
