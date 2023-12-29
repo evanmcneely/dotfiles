41558c4a48d5bcb146100fcaa017030930f1503f
Evan McNeely <evanmcneely@me.com>
2023-12-29 11:01:14 -0800

    better harpoon navigation

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -3,8 +3,10 @@ return {
     "ThePrimeagen/harpoon",
     --stylua: ignore
     keys = {
-      { "<leader>a", function() require("harpoon.mark").add_file() end, desc = "Harpoon Add File" },
-      { "<leader>m", function() require("harpoon.ui").toggle_quick_menu() end, desc = "Harpoon File Menu" },
+      { "<leader>a", function() require("harpoon.mark").add_file() end, desc = "Harpoon add" },
+      { "<leader>m", function() require("harpoon.ui").toggle_quick_menu() end, desc = "Harpoon menu" },
+      { "<leader>h", function() require("harpoon.ui").nav_prev() end, desc = "Harpoon prev" },
+      { "<leader>l", function() require("harpoon.ui").nav_next() end, desc = "Harpoon next" },
       { "<leader>1", function() require("harpoon.ui").nav_file(1) end, desc = "File 1" },
       { "<leader>2", function() require("harpoon.ui").nav_file(2) end, desc = "File 2" },
       { "<leader>3", function() require("harpoon.ui").nav_file(3) end, desc = "File 3" },
