0b989193decd56ee4c623c1f61f82e64bd293f7f
Evan McNeely <evanmcneely@me.com>
2023-12-28 20:50:15 -0800

    disable pyright type checking

--- nvim/lua/plugins/overrides.lua
+++ nvim/lua/plugins/overrides.lua
@@ -115,4 +115,17 @@ return {
       } }
     end,
   },
+
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        pyright = {
+          -- TODO: use neoconf for local setup
+          reportGeneralTypeIssues = false, -- we don't use types at Leadpages
+          -- reportUnboundVariable = false, -- not working
+        },
+      },
+    },
+  },
 }
