729e44fcfc604a406c37d2071551b541adc95acc
Evan McNeely <evan.mcneely@leadpages.com>
2024-01-16 17:28:56 -0800

    nord theme

--- nvim/.config/nvim/lua/plugins/theme.lua
+++ nvim/.config/nvim/lua/plugins/theme.lua
@@ -1,7 +1,46 @@
 return {
-  { "ellisonleao/gruvbox.nvim", priority = 1000, config = true },
-  { "navarasu/onedark.nvim", enabled = false, priority = 1000 },
-  { "catppuccin/nvim", priority = 1000 },
+  {
+    "navarasu/onedark.nvim",
+    lazy = false,
+    -- enabled = false,
+    priority = 1000,
+    opts = {
+      style = "deep",
+    },
+  },
+  {
+    "catppuccin/nvim",
+    enabled = false,
+    lazy = false,
+    priority = 1000,
+  },
+  {
+    "AlexvZyl/nordic.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = function()
+      local palette = require "nordic.colors"
+      local opts = {
+        bold_keywords = true,
+        telescope = {
+          style = "classic",
+        },
+        override = {
+          TelescopePromptTitle = { fg = palette.orange.dim },
+          TelescopeResultsTitle = { fg = palette.orange.dim },
+          TelescopePreviewTitle = { fg = palette.blue2 },
+          TelescopePromptBorder = { fg = palette.orange.dim },
+          TelescopeResultsBorder = { fg = palette.orange.dim },
+          TelescopePreviewBorder = { fg = palette.blue2 },
+          MatchParen = { underline = false },
+          -- Normal = { bg = "#1c1f27" },
+          -- NormalSB = { bg = "red" },
+          -- SignColumnSB = { bg = "#1c1f27" },
+        },
+      }
+      return opts
+    end,
+  },
   {
     "folke/tokyonight.nvim",
     opts = {
@@ -10,23 +49,10 @@ return {
   },
 
   -- Configure LazyVim colorscheme with this
-  -- {
-  --   "LazyVim/LazyVim",
-  --   opts = {
-  --     colorscheme = "onedark",
-  --   },
-  -- },
-
-  -- use colors for specific filestypes
   {
-    "folke/styler.nvim",
-    enabled = false, -- causes "unthemed content"
-    event = "VeryLazy", -- required
+    "LazyVim/LazyVim",
     opts = {
-      themes = {
-        markdown = { colorscheme = "catppuccin" },
-        help = { colorscheme = "gruvbox" },
-      },
+      colorscheme = "nordic",
     },
   },
 }
