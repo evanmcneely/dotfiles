d4a571b3a3343aa27190f01425c554d5ceb12481
Evan McNeely <evan.mcneely@leadpages.com>
2024-01-27 11:52:03 -0800

    change font size from 14 - 13

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -10,23 +10,16 @@ if wezterm.config_builder then
 end
 
 -- config.colors = { background = "#1d2029", cursor_bg = "#bdbfbe", cursor_fg = "#212121" }
--- config.color_scheme = "Night Owl (Gogh)"
 -- config.color_scheme = "nord"
 config.color_scheme = "tokyonight"
 config.font = wezterm.font("Fira Code")
-config.font_size = 14
+config.font_size = 13
 config.line_height = 1.3
-
-config.keys = {
-	-- { key = "LeftArrow", mods = "CMD", action = act.ActivateTabRelative(-1) },
-	-- { key = "RightArrow", mods = "CMD", action = act.ActivateTabRelative(1) },
-}
 
 -- will manually check and download updates
 config.check_for_updates = false
 
 config.window_decorations = "RESIZE"
--- config.use_dead_keys = false
 config.enable_tab_bar = false
 
 return config
