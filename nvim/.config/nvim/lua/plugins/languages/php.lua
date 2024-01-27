5a0dc8f984634b16062b8e4ba52ff8bcf13cd832
Evan McNeely <evan.mcneely@leadpages.com>
2024-01-27 07:40:32 -0800

    php debug setup

--- nvim/.config/nvim/lua/plugins/languages/php.lua
+++ nvim/.config/nvim/lua/plugins/languages/php.lua
@@ -115,4 +115,27 @@ return {
       },
     },
   },
+  {
+    "mfussenegger/nvim-dap",
+    opts = function()
+      local dap = require "dap"
+      dap.adapters.php = {
+        type = "executable",
+        command = "node",
+        args = { os.getenv "HOME" .. "/dev/vscode-php-debug/out/phpDebug.js" },
+      }
+      dap.configurations.php = {
+        {
+          type = "php",
+          request = "launch",
+          name = "Listen for Xdebug",
+          port = 9003,
+          pathMappings = {
+            ["/var/www/html/wp-content/plugins/leadpages"] = "${workspaceFolder}/leadpages/",
+            ["/var/www/html/wp-content/plugins/leadpages/tests"] = "${workspaceFolder}/tests/",
+          },
+        },
+      }
+    end,
+  },
 }
