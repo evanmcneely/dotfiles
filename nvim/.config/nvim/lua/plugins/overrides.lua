561d38efed3406c561087d5ce207c53be4ec9b0a
Evan McNeely <evan.mcneely@leadpages.com>
2024-03-19 15:31:18 -0700

    neotree version 3

--- nvim/.config/nvim/lua/plugins/overrides.lua
+++ nvim/.config/nvim/lua/plugins/overrides.lua
@@ -7,7 +7,7 @@ return {
         "s1n7ax/nvim-window-picker",
         name = "window-picker",
         event = "VeryLazy",
-        version = "2.*",
+        version = "3.*",
         config = function()
           require("window-picker").setup {
             hint = "floating-big-letter",
