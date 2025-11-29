3ec2f72a08a1a5025244813c1444e06e26174ce4
Evan McNeely <evan.mcneely@leadpages.com>
2025-11-28 18:28:27 -0800

    nvim: add supermaven

--- nvim/.config/nvim/lua/plugins/coding.lua
+++ nvim/.config/nvim/lua/plugins/coding.lua
@@ -127,4 +127,18 @@ return {
     ft = "python",
     keys = { { "<leader>cv", "<cmd>:VenvSelect<cr>", desc = "Select VirtualEnv", ft = "python" } },
   },
+
+  {
+    "supermaven-inc/supermaven-nvim",
+    lazy = false,
+    config = function()
+      require("supermaven-nvim").setup {
+        keymaps = {
+          accept_suggestion = "<C-c>",
+          clear_suggestion = "<C-x>",
+          -- accept_word = "<C-j>",
+        },
+      }
+    end,
+  },
 }
