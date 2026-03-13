56cbaec4b78d749df71e93e82c7938e87f832167
Evan McNeely <evan.mcneely@leadpages.com>
2026-03-12 20:21:00 -0700

    nvim: some cleanup

--- nvim/.config/nvim/lua/plugins/coding.lua
+++ nvim/.config/nvim/lua/plugins/coding.lua
@@ -19,25 +19,10 @@ return {
         end,
       }
     end,
+    -- stylua: ignore
     keys = {
-      {
-        "<Tab>",
-        function()
-          return vim.snippet.active { direction = 1 } and "<cmd>lua vim.snippet.jump(1)<cr>" or "<Tab>"
-        end,
-        expr = true,
-        silent = true,
-        mode = { "i", "s" },
-      },
-      {
-        "<S-Tab>",
-        function()
-          return vim.snippet.active { direction = -1 } and "<cmd>lua vim.snippet.jump(-1)<cr>" or "<S-Tab>"
-        end,
-        expr = true,
-        silent = true,
-        mode = { "i", "s" },
-      },
+      { "<Tab>", function() return vim.snippet.active { direction = 1 } and "<cmd>lua vim.snippet.jump(1)<cr>" or "<Tab>" end, expr = true, silent = true, mode = { "i", "s" } },
+      { "<S-Tab>", function() return vim.snippet.active { direction = -1 } and "<cmd>lua vim.snippet.jump(-1)<cr>" or "<S-Tab>" end, expr = true, silent = true, mode = { "i", "s" } },
     },
   },
 
